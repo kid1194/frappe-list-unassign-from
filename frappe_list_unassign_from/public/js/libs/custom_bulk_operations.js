5f6e272d4840866994d0d2d5f5afed2a47068274
Ameen Ahmed <kid_1194@hotmail.com>
2022-08-12 21:32:57 +0300

    Initial release

---
+++ frappe_list_unassign_from/public/js/libs/custom_bulk_operations.js
@@ -0,0 +1,24 @@
+import UnassignFromDialog from './unassign_from_dialog';
+
+export default class CustomBulkOperations {
+    constructor({ doctype }) {
+        if (!doctype) frappe.throw(__('Doctype required'));
+        this.doctype = doctype;
+    }
+    // unassignment
+    unassign (docnames, done) {
+        if (docnames.length > 0) {
+            const unassign_from = new UnassignFromDialog({
+                obj: this,
+                method: 'frappe_list_unassign_from.api.unassign_from.remove_multiple',
+                doctype: this.doctype,
+                docname: docnames,
+                callback: done
+            });
+            unassign_from.dialog.clear();
+            unassign_from.dialog.show();
+        } else {
+            frappe.msgprint(__('Select records to remove assignment'));
+        }
+    }
+}

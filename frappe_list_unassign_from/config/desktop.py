9d79f613169ca4a772c1d8abafd482a8d6e5a290
Ameen Ahmed <kid_1194@hotmail.com>
2022-08-12 21:29:14 +0300

    Initial release

---
+++ frappe_list_unassign_from/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+return [
+    {
+        "module_name": "Frappe List Unassign From",
+        "color": "blue",
+        "icon": "octicon octicon-table",
+        "type": "module",
+        "label": _("Frappe List Unassign From")
+    }
+]

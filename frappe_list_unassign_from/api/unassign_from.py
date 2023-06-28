d6173117cbad525c8530f54dde5962614f43e457
kid1194 <kid1194@gmail.com>
2023-06-29 01:56:16 +0300

    Update to v1.0.7

--- frappe_list_unassign_from/api/unassign_from.py
+++ frappe_list_unassign_from/api/unassign_from.py
@@ -5,43 +5,64 @@
 
 
 import frappe
-from frappe.query_builder.functions import Locate
-from pypika.enums import Order
 from frappe.desk.form.assign_to import set_status
 
 
 @frappe.whitelist()
 @frappe.validate_and_sanitize_search_inputs
-def search_link(doctype, txt, searchfield, start, page_len, filters):
+def search_link(doctype, txt, searchfield, start, page_length, filters):
     if not hasattr(filters, "docname"):
         return []
     
-    dToDo = frappe.qb.DocType("ToDo")
-    qTodo = (
-        frappe.qb.from_(dToDo)
-        .where(dToDo.reference_type == doctype)
-        .where(dToDo.reference_name.isin(filters.get("docname")))
-        .limit(page_len)
-        .offset(start)
+    dt = "ToDo"
+    fields = []
+    fields.append("""`tab{doctype}`.`allocated_to`""".format(doctype=dt))
+    
+    filters = []
+    filters.append([dt, "reference_type", "=", doctype])
+    filters.append([dt, "reference_name", "in", filters.get("docname")])
+    
+    order_by = None
+    
+    if txt:
+        fields.append("""locate({_txt}, `tab{doctype}`.`name`) as `_relevance`""".format(
+            _txt=frappe.db.escape((txt or "").replace("%", "").replace("@", "")),
+            doctype=dt
+        ))
+        
+        filters.append([dt, "allocated_to", "like", "%{0}%".format(txt)])
+        
+        order_by = "_relevance desc"
+    
+    data = frappe.get_list(
+        dt,
+        fields=fields,
+        filters=filters,
+        limit_start=start,
+        limit_page_length=page_length,
+        order_by=order_by,
+        ignore_permissions=True,
+        as_list=False
     )
     
-    if not txt:
-        qTodo.select(dToDo.allocated_to)
-    else:
-        qTodo.select(dToDo.allocated_to, Locate(txt, dToDo.allocated_to).as_("_relevance"))
-        qTodo.where(dToDo.allocated_to.like(f"%{txt}%"))
-        qTodo.orderby("_relevance", order=Order.desc)
+    if data:
+        users = [[v.allocated_to, v.allocated_to] for v in data]
+        return users
     
-    data = qTodo.run(as_dict=True)
-    users = [[v.allocated_to, v.allocated_to] for v in data]
-    
-    return users
+    return []
 
 
 @frappe.whitelist()
 def remove_multiple(args=None):
     if not args:
         args = frappe.local.form_dict
+    
+    if (
+        not hasattr(args, "doctype") or
+        not hasattr(args, "name") or
+        not hasattr(args, "unassign_from")
+    ):
+        return 0
 
     docname_list = frappe.parse_json(args.get("name"))
     unassign_from_list = frappe.parse_json(args.get("unassign_from"))
@@ -49,3 +70,5 @@ def remove_multiple(args=None):
     for docname in docname_list:
         for unassign_from in unassign_from_list:
             set_status(args.get("doctype"), docname, unassign_from, status="Cancelled")
+    
+    return 1
